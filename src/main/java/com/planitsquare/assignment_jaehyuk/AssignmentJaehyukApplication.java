b4b60ff8d2d8219e8e23de197af943a086d34f54
jaehyuk <jaehyuk.dev@gmail.com>
2025-07-07 21:49:40 +0900

    [FIX] Page 직렬화 안정성 개선을 위한 SpringDataWebSupport 설정 추가

--- src/main/java/com/planitsquare/assignment_jaehyuk/AssignmentJaehyukApplication.java
+++ src/main/java/com/planitsquare/assignment_jaehyuk/AssignmentJaehyukApplication.java
@@ -2,8 +2,12 @@ package com.planitsquare.assignment_jaehyuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class AssignmentJaehyukApplication {
 
 	public static void main(String[] args) {
