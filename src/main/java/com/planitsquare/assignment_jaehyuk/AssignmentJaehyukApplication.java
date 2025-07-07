33e1708947a3da7e22c406bc95d203003ae65f3c
jaehyuk <jaehyuk.dev@gmail.com>
2025-07-07 18:59:29 +0900

    init

---
+++ src/main/java/com/planitsquare/assignment_jaehyuk/AssignmentJaehyukApplication.java
@@ -0,0 +1,13 @@
+package com.planitsquare.assignment_jaehyuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJaehyukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentJaehyukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planitsquare/assignment_jaehyuk/AssignmentJaehyukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitsquare.assignment_jaehyuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJaehyukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
